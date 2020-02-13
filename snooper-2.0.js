83a634f1fd1e1b81e9e5ec161a44a61a8ef2e0cc
NathanGassaway <Nathangway@gmail.com>
2020-02-13 12:22:58 -0500

    Add files via upload

---
+++ snooper-2.0.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	openProject = function(){
+	 	$(".landing-image").click(function(){
+	 		$(".landing-image").fadeOut(200, function(){
+	 			$(".left-panel").fadeIn(200);
+	 		$(".right-panel").fadeIn(200);
+	 			});
+	 	});
+ 	}
+
+ 	orderButton = function(){
+ 		$(".purchase").click(function(){
+ 			$(".button-form").submit()
+ 		});
+
+ 	}
+
+ 	subscribeButton = function(){
+ 		$(".subscribe").click(function(){
+ 			$(".validate").submit()
+ 		});
+ 	}
+
+ 	openProject();
+
+ 	orderButton();
+
+ 	subscribeButton();
+
+});
