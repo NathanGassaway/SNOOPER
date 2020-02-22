870fa184690dc2b26777ff67ab91eb76bf0111b4
NathanGassaway <Nathangway@gmail.com>
2020-02-22 17:22:57 +0100

    Add files via upload

--- snooper-2.0.js
+++ snooper-2.0.js
@@ -2,9 +2,10 @@ $(document).ready(function(){
 
 	openProject = function(){
 	 	$(".landing-image").click(function(){
-	 		$(".landing-image").fadeOut(200, function(){
+	 		$(".landing").fadeOut(200, function(){
 	 			$(".left-panel").fadeIn(200);
 	 		$(".right-panel").fadeIn(200);
+	 		$(".panel").fadeIn(200);
 	 			});
 	 	});
  	}
